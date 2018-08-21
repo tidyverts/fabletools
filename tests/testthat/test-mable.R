ff341fa78ede7f0fa6ed33f73ae4f579edab7eea
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-21 16:06:23 +1000

    Added mable test

---
+++ tests/testthat/test-mable.R
@@ -0,0 +1,44 @@
+context("test-mable.R")
+
+test_that("Mable classes", {
+  expect_s3_class(mbl, c("mable", "lst_ts"))
+  expect_s3_class(mbl[["model"]], "lst_mdl")
+})
+
+test_that("Mable print output", {
+  expect_output(print(mbl), "A mable: 1 model \\[1M\\]")
+})
+
+test_that("Mable fitted values", {
+  fits <- fitted(mbl)
+  expect_true(is_tsibble(fits))
+  expect_equal(colnames(fits), c("index", "fitted"))
+  expect_equal(fits[["index"]], USAccDeaths[["index"]])
+  expect_equal(fits[["fitted"]], as.numeric(fitted(mbl[["model"]][[1]])))
+  
+  fits <- fitted(mbl_multi)
+  expect_true(is_tsibble(fits))
+  expect_equal(key_vars(fits), "key")
+  expect_equal(colnames(fits), c("key", "index", "fitted"))
+  expect_equal(unique(fits[[key_vars(fits)]]), c("fdeaths", "mdeaths"))
+  expect_equal(fits[["index"]], UKLungDeaths[["index"]])
+  # https://github.com/tidyverts/tsibble/issues/56
+  # expect_equal(fits[["fitted"]], as.numeric(c(fitted(mbl_multi[["model"]][[1]]),fitted(mbl_multi[["model"]][[2]]))))
+})
+
+test_that("Mable residuals", {
+  resids <- residuals(mbl)
+  expect_true(is_tsibble(resids))
+  expect_equal(colnames(resids), c("index", "residuals"))
+  expect_equal(resids[["index"]], USAccDeaths[["index"]])
+  expect_equal(resids[["residuals"]], as.numeric(residuals(mbl[["model"]][[1]])))
+  
+  resids <- residuals(mbl_multi)
+  expect_true(is_tsibble(resids))
+  expect_equal(key_vars(resids), "key")
+  expect_equal(colnames(resids), c("key", "index", "residuals"))
+  expect_equal(unique(resids[[key_vars(resids)]]), c("fdeaths", "mdeaths"))
+  expect_equal(resids[["index"]], UKLungDeaths[["index"]])
+  # https://github.com/tidyverts/tsibble/issues/56
+  # expect_equal(resids[["residuals"]], as.numeric(c(residuals(mbl_multi[["model"]][[1]]),residuals(mbl_multi[["model"]][[2]]))))
+})
