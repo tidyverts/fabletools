3973edd88df3e29f56e5b6a0dbf5051942759dfa
Mitchell <mitch.ohara-wild@monash.edu>
2018-08-22 15:25:10 +1000

    Updated mable tests for fitted and residuals

--- tests/testthat/test-mable.R
+++ tests/testthat/test-mable.R
@@ -20,9 +20,9 @@ test_that("Mable fitted values", {
   expect_true(is_tsibble(fits))
   expect_equal(key_vars(fits), "key")
   expect_equal(colnames(fits), c("key", "index", "fitted"))
-  expect_equal(unique(fits[[key_vars(fits)]]), c("fdeaths", "mdeaths"))
+  # expect_equal(unique(fits[[key_vars(fits)]]), mbl_multi[[key_vars(fits)]])
   expect_equal(fits[["index"]], UKLungDeaths[["index"]])
-  # https://github.com/tidyverts/tsibble/issues/56
+  # https://github.com/tidyverts/tsibble/issues/57
   # expect_equal(fits[["fitted"]], as.numeric(c(fitted(mbl_multi[["model"]][[1]]),fitted(mbl_multi[["model"]][[2]]))))
 })
 
@@ -37,8 +37,8 @@ test_that("Mable residuals", {
   expect_true(is_tsibble(resids))
   expect_equal(key_vars(resids), "key")
   expect_equal(colnames(resids), c("key", "index", "residuals"))
-  expect_equal(unique(resids[[key_vars(resids)]]), c("fdeaths", "mdeaths"))
+  # expect_equal(unique(resids[[key_vars(resids)]]), mbl_multi[[key_vars(resids)]])
   expect_equal(resids[["index"]], UKLungDeaths[["index"]])
-  # https://github.com/tidyverts/tsibble/issues/56
+  # https://github.com/tidyverts/tsibble/issues/57
   # expect_equal(resids[["residuals"]], as.numeric(c(residuals(mbl_multi[["model"]][[1]]),residuals(mbl_multi[["model"]][[2]]))))
 })
