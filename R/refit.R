157d111d727ae434b4b57814b19c477999f34465
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-02 11:23:00 +1000

    Added refit()

---
+++ R/refit.R
@@ -0,0 +1,22 @@
+#' Refit a model with new data
+#' 
+#' @param object A model (typically a mable)
+#' @param new_data A tsibble containing the new data
+#' @param ... Additional optional arguments for refit methods
+#' 
+#' @export
+refit <- function(object, new_data, ...){
+  UseMethod("refit")
+}
+
+#' @export
+refit.mable <- function(object, new_data, ...){
+  key <- syms(key_vars(object))
+  object <- bind_new_data(object, new_data)
+  names(object)[names(object) == "model"] <- "object"
+
+  out <- map2(object$object, object$new_data, refit, ...) %>% 
+    invoke("rbind", .)
+  
+  add_class(bind_cols(object[map_chr(key, expr_text)], out), class(out))
+}
