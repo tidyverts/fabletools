9d15c05817edfaf353bfd6179b57faf2eb0ed3da
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-16 11:41:35 +1100

    fitted() and residuals() now expect models to return a tsibble
    Preparation for removing the data column from models

--- R/fitted.R
+++ R/fitted.R
@@ -4,15 +4,7 @@ fitted.mable <- function(object, ...){
   keys <- syms(key_vars(object))
   object %>%
     transmute(!!!keys,
-              fitted = map2(!!sym("data"), !!sym("model"),
-                            function(data, model) {
-                              data %>% transmute(fitted = as.numeric(fitted(model)))
-                            }
-              )
+              fitted = map(!!sym("model"), fitted)
     ) %>%
     unnest(key = keys)
 }
-
-#' @importFrom stats fitted
-#' @export
-fitted.fable <- fitted.mable
--- R/residuals.R
+++ R/residuals.R
@@ -4,16 +4,7 @@ residuals.mable <- function(object, ...){
   keys <- syms(key_vars(object))
   object %>%
     transmute(!!!keys,
-              residuals = map2(!!sym("data"), !!sym("model"),
-                               function(data, model) {
-                                 data %>% transmute(residuals = data[[expr_text((model%@%"fable")$response)]] - as.numeric(fitted(model)))
-                               }
-              )
-    )%>%
+              residuals = map(!!sym("model"), residuals)
+    ) %>%
     unnest(key = keys)
 }
-
-
-#' @importFrom stats residuals
-#' @export
-residuals.fable <- residuals.mable
