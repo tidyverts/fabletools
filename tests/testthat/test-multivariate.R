dad57426de6c280bf6f6250ac0ef6383a78319e1
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-17 12:38:29 +1100

    Updated expected forecast object for batch forecasting

--- tests/testthat/test-multivariate.R
+++ tests/testthat/test-multivariate.R
@@ -4,6 +4,6 @@ test_that("multiple univariate", {
   expect_equal(sort(mbl_multi$key), c("fdeaths", "mdeaths"))
   expect_s3_class(mbl_multi$model, "lst_mdl")
   
-  expect_equal(sort(fbl_multi$key), c("fdeaths", "mdeaths"))
-  expect_s3_class(fbl_multi$forecast, "lst_fc")
+  expect_equal(sort(unique(fbl_multi$key)), c("fdeaths", "mdeaths"))
+  expect_s3_class(fbl_multi$distribution, "fcdist")
 })
