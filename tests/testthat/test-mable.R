656f1447533dbbf286d5a4cd2b683a4ffb09546b
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-17 12:36:00 +1100

    Updated expected objects for fitted() and residuals()

--- tests/testthat/test-mable.R
+++ tests/testthat/test-mable.R
@@ -12,33 +12,41 @@ test_that("Mable print output", {
 test_that("Mable fitted values", {
   fits <- fitted(mbl)
   expect_true(is_tsibble(fits))
-  expect_equal(colnames(fits), c("index", "fitted"))
+  expect_true(all(colnames(fits) %in% c("index", ".fitted")))
   expect_equal(fits[["index"]], USAccDeaths[["index"]])
-  expect_equal(fits[["fitted"]], as.numeric(fitted(mbl[["model"]][[1]])))
+  expect_equal(fits[[".fitted"]], fitted(mbl[["model"]][[1]])[[".fitted"]])
   
   fits <- fitted(mbl_multi)
   expect_true(is_tsibble(fits))
   expect_equal(key_vars(fits), "key")
-  expect_equal(colnames(fits), c("key", "index", "fitted"))
-  # expect_equal(unique(fits[[key_vars(fits)]]), mbl_multi[[key_vars(fits)]])
+  expect_true(all(colnames(fits) %in% c("key", "index", ".fitted")))
+  expect_equal(unique(fits[[key_vars(fits)]]), mbl_multi[[key_vars(fits)]])
   expect_equal(fits[["index"]], UKLungDeaths[["index"]])
-  # https://github.com/tidyverts/tsibble/issues/57
-  # expect_equal(fits[["fitted"]], as.numeric(c(fitted(mbl_multi[["model"]][[1]]),fitted(mbl_multi[["model"]][[2]]))))
+  expect_equal(fits[[".fitted"]],
+               as.numeric(c(
+                 fitted(mbl_multi[["model"]][[1]])[[".fitted"]],
+                 fitted(mbl_multi[["model"]][[2]])[[".fitted"]]
+               ))
+  )
 })
 
 test_that("Mable residuals", {
   resids <- residuals(mbl)
   expect_true(is_tsibble(resids))
-  expect_equal(colnames(resids), c("index", "residuals"))
+  expect_true(all(colnames(resids) %in% c("index", ".resid")))
   expect_equal(resids[["index"]], USAccDeaths[["index"]])
-  expect_equal(resids[["residuals"]], as.numeric(residuals(mbl[["model"]][[1]])))
+  expect_equal(resids[[".resid"]], as.numeric(residuals(mbl[["model"]][[1]])[[".resid"]]))
   
   resids <- residuals(mbl_multi)
   expect_true(is_tsibble(resids))
   expect_equal(key_vars(resids), "key")
-  expect_equal(colnames(resids), c("key", "index", "residuals"))
-  # expect_equal(unique(resids[[key_vars(resids)]]), mbl_multi[[key_vars(resids)]])
+  expect_true(all(colnames(resids) %in% c("key", "index", ".resid")))
+  expect_equal(unique(resids[[key_vars(resids)]]), mbl_multi[[key_vars(resids)]])
   expect_equal(resids[["index"]], UKLungDeaths[["index"]])
-  # https://github.com/tidyverts/tsibble/issues/57
-  # expect_equal(resids[["residuals"]], as.numeric(c(residuals(mbl_multi[["model"]][[1]]),residuals(mbl_multi[["model"]][[2]]))))
+  expect_equal(resids[[".resid"]], 
+               as.numeric(c(
+                 residuals(mbl_multi[["model"]][[1]])[[".resid"]],
+                 residuals(mbl_multi[["model"]][[2]])[[".resid"]]
+               ))
+  )
 })
