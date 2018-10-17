72151cd3769c0e3c523002570c37bcd12bdd766d
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-17 12:41:38 +1100

    Replaced summary test with hilo calculation for new forecast structure

---
+++ tests/testthat/test-hilo.R
@@ -0,0 +1,8 @@
+context("test-hilo.R")
+
+test_that("Extracting intervals from a distribution", {
+  fc_sum <- fbl %>% 
+    mutate("80%" := hilo(distribution, 80))
+  
+  expect_s3_class(fc_sum$`80%`, "hilo")
+})
--- tests/testthat/test-summary.R
+++
@@ -1,8 +0,0 @@
-context("test-summary.R")
-
-test_that("Summarising mables / fables", {
-  fc_sum <- fbl %>% summary
-  
-  expect_equal(dim(fc_sum), c(24, 5))
-  expect_s3_class(fc_sum$`80%`, "hilo")
-})
