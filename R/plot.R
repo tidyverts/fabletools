0eac0808cb1d0623457e6783ce90047f0b85e671
Mitchell <mitch.ohara-wild@monash.edu>
2018-10-18 14:19:25 +1100

    Added autolayer.tbl_ts

--- R/plot.R
+++ R/plot.R
@@ -22,6 +22,28 @@ autoplot.tbl_ts <- function(object, var = NULL, ...){
     xlab(paste0(expr_text(index(object)), " [", format(interval(object)), "]"))
 }
 
+#' @importFrom ggplot2 ggplot aes geom_line guides guide_legend xlab
+#' @export
+autolayer.tbl_ts <- function(object, var = NULL, ...){
+  if(quo_is_null(enquo(var))){
+    inform(sprintf(
+      "Plot variable not specified, automatically selected `var = %s`",
+      measured_vars(object)[1]
+    ))
+    var <- sym(measured_vars(object)[1])
+  }
+  else{
+    var <- enexpr(var)
+  }
+  
+  aes_spec <- list(x = index(object), y = var)
+  if(n_keys(object) > 1){
+    aes_spec["colour"] <- list(expr(interaction(!!!syms(key_vars(object)), sep = "/")))
+  }
+  
+  geom_line(eval_tidy(expr(aes(!!!aes_spec))), data = object)
+}
+
 #' @export
 autoplot.mable <- function(object, ...){
   if(length(object$model) > 1){
@@ -134,7 +156,6 @@ autoplot.fbl_ts <- function(object, level = c(80, 95), ...){
 autolayer.fbl_ts <- function(object, level = c(80, 95), series = NULL, ...){
   data <- fortify(object, level = level, ...)
   mapping <- eval_tidy(quo(aes(x = !!index(data), y = !!sym("mean"))))
-  browser()
   if(!is.null(level)){
     mapping$level <- sym("level")
     mapping$ymin <- sym("lower")
