1ccf0784ce0f9a7f0779541fedf273a65b4a9bd5
Mitchell <mitch.ohara-wild@monash.edu>
2018-11-06 11:43:02 +1100

    Added point_measures as default accuracy measures - named set of fns

--- R/accuracy.R
+++ R/accuracy.R
@@ -1,3 +1,84 @@
+#' Point estimate accuracy measures
+#' 
+#' @param .resid A vector of residuals from either the training (model accuracy) or test (forecast accuracy) data.
+#' @param .resp A vector of responses matching the residuals (for forecast accuracy, the original data must be provided).
+#' @param .period The seasonal period of the data (defaulting to 'smallest' seasonal period).
+# #' @param .fitted The fitted values from the model, or forecasted values from the forecast.
+# #' @param .dist The distribution of fitted values from the model, or forecasted values from the forecast.
+# #' @param .expr_resp An expression for the response variable.
+#' @param na.rm Remove the missing values before calculating the accuracy measure
+#' @param ... Additional arguments for each measure.
+#' @param demean Should the response be demeaned (MASE)
+#' @param d Should the response model include a first difference?
+#' @param D Should the response model include a seasonal difference?
+#' @param na.action Function to handle missing values.
+#' 
+#' @name point-accuracy-measures
+NULL
+
+#' @rdname point-accuracy-measures
+#' @export
+ME <- function(.resid, na.rm = TRUE, ...){
+  mean(.resid, na.rm = na.rm, ...)
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+MSE <- function(.resid, na.rm = TRUE, ...){
+  mean(.resid ^ 2, na.rm = na.rm, ...)
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+RMSE <- function(.resid, na.rm = TRUE, ...){
+  sqrt(MSE(.resid, na.rm = na.rm, ...))
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+MAE <- function(.resid, na.rm = TRUE, ...){
+  mean(abs(.resid), na.rm = na.rm, ...)
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+MPE <- function(.resid, .resp, na.rm = TRUE, ...){
+  mean(.resid / .resp * 100, na.rm = TRUE, ...)
+}
+#' @rdname point-accuracy-measures
+#' @export
+MAPE <- function(.resid, .resp, na.rm = TRUE, ...){
+  mean(abs(.resid / .resp * 100), na.rm = TRUE, ...)
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+MASE <- function(.resid, .resp, demean = FALSE, na.rm = TRUE, .period, d = .period == 1, D = .period > 1, ...){
+  if (D > 0) { # seasonal differencing
+    y <- diff(.resp, lag = .period, differences = D)
+  }
+  if (d > 0) {
+    y <- diff(y, differences = d)
+  }
+  if(demean){
+    scale <- mean(abs(y - mean(y, na.rm = na.rm, ...)), na.rm = na.rm, ...)
+  }
+  else{
+    scale <- mean(abs(y), na.rm = na.rm, ...)
+  }
+  mase <- mean(abs(.resid / scale), na.rm = na.rm, ...)
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+ACF1 <- function(.resid, na.action = stats::na.pass, ...){
+  stats::acf(.resid, plot = FALSE, lag.max = 2, na.action = na.action, ...)$acf[2, 1, 1]
+}
+
+#' @rdname point-accuracy-measures
+#' @export
+point_measures <- list(ME = ME, RMSE = RMSE, MAPE = MAPE)
+
 #' Evaluate model/forecast accuracy
 #' 
 #' @param x A model or forecast object
@@ -20,7 +101,7 @@ accuracy <- function(x, ...){
 }
 
 #' @export
-accuracy.mdl_df <- function(x, measures = list(ME, RMSE, MAPE), ...){
+accuracy.mdl_df <- function(x, measures = list(point_measures), ...){
   dots <- dots_list(...)
   aug <- augment(x)
   measures <- squash(measures)
@@ -59,79 +140,4 @@ accuracy.mdl_df <- function(x, measures = list(ME, RMSE, MAPE), ...){
     summarise(
       !!!compact(fns)
     )
-}
-
-#' Point estimate accuracy measures
-#' 
-#' @param .resid A vector of residuals from either the training (model accuracy) or test (forecast accuracy) data.
-#' @param .resp A vector of responses matching the residuals (for forecast accuracy, the original data must be provided).
-#' @param .period The seasonal period of the data (defaulting to 'smallest' seasonal period).
-# #' @param .fitted The fitted values from the model, or forecasted values from the forecast.
-# #' @param .dist The distribution of fitted values from the model, or forecasted values from the forecast.
-# #' @param .expr_resp An expression for the response variable.
-#' @param na.rm Remove the missing values before calculating the accuracy measure
-#' @param ... Additional arguments for each measure.
-#' @param demean Should the response be demeaned (MASE)
-#' @param d Should the response model include a first difference?
-#' @param D Should the response model include a seasonal difference?
-#' @param na.action Function to handle missing values.
-#' 
-#' @rdname point-accuracy-measures
-#' @export
-ME <- function(.resid, na.rm = TRUE, ...){
-  mean(.resid, na.rm = na.rm, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-MSE <- function(.resid, na.rm = TRUE, ...){
-  mean(.resid ^ 2, na.rm = na.rm, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-RMSE <- function(.resid, na.rm = TRUE, ...){
-  sqrt(MSE(.resid, na.rm = na.rm, ...))
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-MAE <- function(.resid, na.rm = TRUE, ...){
-  mean(abs(.resid), na.rm = na.rm, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-MPE <- function(.resid, .resp, na.rm = TRUE, ...){
-  mean(.resid / .resp * 100, na.rm = TRUE, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-MAPE <- function(.resid, .resp, na.rm = TRUE, ...){
-  mean(abs(.resid / .resp * 100), na.rm = TRUE, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-MASE <- function(.resid, .resp, demean = FALSE, na.rm = TRUE, .period, d = .period == 1, D = .period > 1, ...){
-  if (D > 0) { # seasonal differencing
-    y <- diff(.resp, lag = .period, differences = D)
-  }
-  if (d > 0) {
-    y <- diff(y, differences = d)
-  }
-  if(demean){
-    scale <- mean(abs(y - mean(y, na.rm = na.rm, ...)), na.rm = na.rm, ...)
-  }
-  else{
-    scale <- mean(abs(y), na.rm = na.rm, ...)
-  }
-  mase <- mean(abs(.resid / scale), na.rm = na.rm, ...)
-}
-
-#' @rdname point-accuracy-measures
-#' @export
-ACF1 <- function(.resid, na.action = stats::na.pass, ...){
-  stats::acf(.resid, plot = FALSE, lag.max = 2, na.action = na.action, ...)$acf[2, 1, 1]
 }
