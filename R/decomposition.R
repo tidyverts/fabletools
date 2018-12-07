af44c2dba709956a04ff3676ae67af49c1c2f0d9
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-07 11:02:50 +1100

    removed model_split

--- R/decomposition.R
+++ R/decomposition.R
@@ -1,11 +1,3 @@
-modelsplit <- function(object, ...){
-  UseMethod("modelsplit")
-}
-
-modelsplit.dable <- function(object, ...){
-  modelsplit(object[["decomp"]], ...)
-}
-
 #' Extract model or decomposition components
 #' 
 #' Extracts the decomposed components from an object, or the states from a state space model.
