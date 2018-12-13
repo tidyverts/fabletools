6d5333f00a4217602ba048113f24ba6777a284d5
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-13 14:58:37 +1100

    Fixed summary method for new mable structure

--- R/mable.R
+++ R/mable.R
@@ -86,9 +86,14 @@ model_sum.default <- function(x){
 #' @importFrom utils head capture.output
 #' @export
 summary.mdl_df <- function(object, ...){
-  map(head(object$model), function(.x) capture.output(summary(.x))) %>%
+  map(head(object[[as_string((object%@%"models")[[1]])]]), function(.x) capture.output(summary(.x))) %>%
     invoke(cat, ., sep="\n")
   invisible(object)
+}
+
+#' @export
+summary.model <- function(object, ...){
+  summary(object$fit, ...)
 }
 
 #' @export
