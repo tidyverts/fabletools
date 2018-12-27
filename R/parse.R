dbdee86b2e1c0cb1dae821d1e00d0085364ac5ae
Mitchell <mitch.ohara-wild@monash.edu>
2018-12-27 11:02:18 +1100

    Fixed binding of variables existing in data for specials evaluation

--- R/parse.R
+++ R/parse.R
@@ -130,7 +130,7 @@ parse_model_rhs <- function(model){
       .x %>%
           map(
             function(special){
-              eval_tidy(special, env = model$specials)
+              eval_tidy(special, data = model$data, env = model$specials)
             }
           )
     }) %>%
