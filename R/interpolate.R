b3cb34c840d3d847609075537d1e6c4f54a5aa9f
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-07 13:25:34 +1100

    Fixed interpolate for new mable checks

--- R/interpolate.R
+++ R/interpolate.R
@@ -4,14 +4,16 @@ interpolate.mdl_df <- function(object, new_data, ...){
 abort("Interpolation can only be done using one model. 
 Please use select() to choose the model to interpolate with.")
   }
+  keys <- key(object)
   object %>%
     bind_new_data(new_data) %>% 
+    as_tibble %>% 
     transmute(
-      !!!key(object),
+      !!!keys,
       interpolated = map2(!!!(object%@%"models"), new_data, interpolate, ...)
     ) %>% 
     add_class("lst_ts") %>% 
-    unnest(key = key(object))
+    unnest(key = keys)
 }
 
 #' @export
