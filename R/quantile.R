0f1662179fc955f1ac5bee0c9c2206b71cc046ec
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-14 17:23:25 +1100

    Fixed format method for fcdist NA

--- R/quantile.R
+++ R/quantile.R
@@ -171,10 +171,11 @@ format_dist <- function(fn_nm){
 
 #' @export
 format.fcdist <- function(x, ...){
-  .env_ids <- map_chr(x, function(x) env_label(x[[length(x)]]))
-  split(x, .env_ids) %>% 
+  .env_ids <- map_chr(x, function(x) possibly(env_label, ".na")(x[[length(x)]]))
+  split(x, .env_ids) %>%
     set_names(NULL) %>% 
     map(function(x){
+      if(!is_env(x[[1]][[length(x[[1]])]])) return("NA")
       out <- x[[1]]$.env$format(map(x, function(x) x[-length(x)]))
       if(x[[1]]$.env$trans){
         out <- paste0("t(", out, ")")
