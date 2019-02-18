39159ccae626c21cc4d4c28988ef7533a2b4ec91
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-18 15:38:57 +1100

    Improved aesthetic passing and colouring for autolayer.tbl_ts

--- R/plot.R
+++ R/plot.R
@@ -24,7 +24,7 @@ autoplot.tbl_ts <- function(object, var = NULL, ...){
 
 #' @importFrom ggplot2 ggplot aes geom_line guides guide_legend xlab
 #' @export
-autolayer.tbl_ts <- function(object, var = NULL, ...){
+autolayer.tbl_ts <- function(object, var = NULL, series = NULL, ...){
   if(quo_is_null(enquo(var))){
     inform(sprintf(
       "Plot variable not specified, automatically selected `var = %s`",
@@ -37,11 +37,14 @@ autolayer.tbl_ts <- function(object, var = NULL, ...){
   }
   
   aes_spec <- list(x = index(object), y = var)
-  if(n_keys(object) > 1){
+  if(!is.null(series)){
+    mapping$colour <- series
+  }
+  else if(n_keys(object) > 1){
     aes_spec["colour"] <- list(expr(interaction(!!!syms(key_vars(object)), sep = "/")))
   }
   
-  geom_line(eval_tidy(expr(aes(!!!aes_spec))), data = object)
+  geom_line(eval_tidy(expr(aes(!!!aes_spec))), data = object, ...)
 }
 
 #' @export
