8083f639ac930ef8051a696f1d4e889c1f2cb300
Mitchell <mitch.ohara-wild@monash.edu>
2019-02-21 15:51:50 +1100

    Added structure for report generic

--- R/report.R
+++ R/report.R
@@ -9,3 +9,18 @@
 report <- function(object, ...){
   UseMethod("report")
 }
+
+#' @export
+report.mdl_df <- function(object, ...){
+  if(NROW(object) > 1 || length(object%@%"models") > 1){
+    glance(object)
+  }
+  else{
+    report(object[[expr_text((object%@%"models")[[1]])]][[1]])
+  }
+}
+
+#' @export
+report.model <- function(object, ...){
+  report(object[["fit"]])
+}
