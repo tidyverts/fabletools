b95ec62c0b66aa20d8c5e9eaa19d39c36ddc298a
Mitchell <mitch.ohara-wild@monash.edu>
2019-03-04 13:55:08 +1100

    Added warning for using report() with more than one model

--- R/report.R
+++ R/report.R
@@ -13,6 +13,7 @@ report <- function(object, ...){
 #' @export
 report.mdl_df <- function(object, ...){
   if(NROW(object) > 1 || length(object%@%"models") > 1){
+    warning("Model reporting is only supported for individual models, so a glance will be shown. To see the report for a specific model, use `select()` and `filter()` to identify a single model.")
     glance(object)
   }
   else{
