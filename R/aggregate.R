bfc8d76a1527d8a72c39c7ccb5547582c230915c
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-20 10:43:55 +1000

    Improved aggregate_keys docs and formals for summarise() consistency

--- R/aggregate.R
+++ R/aggregate.R
@@ -1,32 +1,35 @@
-#' Expand a dataset to include all levels of aggregation
+#' Expand a dataset to include other levels of aggregation
 #' 
-#' All keys must be of the same length over the same time period, and a grouping
-#' aggregation structure will be used.
+#' Uses the structural specification given in `structure` to aggregate a time
+#' series. Commonly used in combination with forecast reconciliation.
 #' 
-#' @param data A dataset to aggregate.
+#' A grouped structure is specified using `grp1 * grp2`, and a nested structure 
+#' is specified via `parent / child`. 
+#' 
+#' @inheritParams dplyr::summarise
 #' @param structure The specification of aggregation structure.
-#' @param ... Further arguments passed to other methods.
 #' 
 #' @examples 
 #' library(tsibble)
-#' aggregate_keys(tourism)
+#' tourism_grp <- tourism %>% 
+#'   aggregate_keys(Purpose * (State / Region), Trips = sum(Trips))
 #' 
 #' @export
-aggregate_keys <- function(data, structure, ...){
+aggregate_keys <- function(.data, structure, ...){
   UseMethod("aggregate_keys")
 }
 
 #' @export
-aggregate_keys.tbl_ts <- function(data, structure = NULL, ...){
+aggregate_keys.tbl_ts <- function(.data, structure = NULL, ...){
   message("Note: reconciliation in fable is highly experimental. The interface will be refined in the near future.")
   
   structure <- enexpr(structure)
   if(is.null(structure)){
     message(
       sprintf("Key structural specification not found, defaulting to `structure = %s`",
-              paste(key_vars(data), collapse = "*"))
+              paste(key_vars(.data), collapse = "*"))
     )
-    structure <- parse_expr(paste(key_vars(data), collapse = "*"))
+    structure <- parse_expr(paste(key_vars(.data), collapse = "*"))
   }
   
   parse_spec <- function(spec){
@@ -63,8 +66,8 @@ aggregate_keys.tbl_ts <- function(data, structure = NULL, ...){
   # Key combinations
   key_comb <- c(list(chr()), parse_spec(structure))
   
-  idx <- index2(data)
-  data <- as_tibble(data)
+  idx <- index2(.data)
+  .data <- as_tibble(.data)
   
   bind_row_attrb <- function(x){
     attrb <- transpose(map(x, function(dt) map(dt, attributes)))
@@ -79,31 +82,17 @@ aggregate_keys.tbl_ts <- function(data, structure = NULL, ...){
   }
   
   agg_dt <- bind_row_attrb(map(key_comb, function(x){
-    group_data(group_by(data, !!idx, !!!syms(x)))
+    group_data(group_by(.data, !!idx, !!!syms(x)))
   }))
   
   kv <- setdiff(colnames(agg_dt), c(as_string(idx), ".rows"))
   agg_dt <- agg_dt[c(as_string(idx), kv, ".rows")]
   
-  data <- new_tibble(data, groups = agg_dt, subclass = "grouped_df")
-  
-  # # Aggregate variables
-  # agg_dt$.rows <- map(agg_dt$.rows, function(comb_rows){
-  #   as_tsibble(c(
-  #     set_names(list(idx[comb_rows[[1]]]), idx_chr),
-  #     map(vars, function(x){
-  #       possibly(reduce, rep(NA, length(comb_rows[[1]])))(
-  #         comb_rows[-1], 
-  #         function(tot, y) tot + x[y], 
-  #         .init = x[comb_rows[[1]]]
-  #       )
-  #     })
-  #   ), index = idx_chr)
-  # })
+  .data <- new_tibble(.data, groups = agg_dt, subclass = "grouped_df")
   
   # Compute aggregates
-  data <- ungroup(summarise(data, ...))
+  .data <- ungroup(summarise(.data, ...))
   
   # Return tsibble
-  as_tsibble(data, key = kv, index = !!idx)
+  as_tsibble(.data, key = kv, index = !!idx)
 }
