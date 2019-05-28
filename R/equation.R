066faff38fc37f20d4d0738553108c8b8c76bd83
Mitchell <mitch.ohara-wild@monash.edu>
2019-05-28 13:36:17 +1000

    Added equation() support for model objects

---
+++ R/equation.R
@@ -0,0 +1,15 @@
+#' @export
+equation.mdl_df <- function(object, ...){
+  if(NROW(object) > 1 || length(object%@%"models") > 1){
+    abort("Model equations are only supported for individual models. To see the equation for a specific model, use `select()` and `filter()` to identify a single model.")
+  }
+  equation(object[[(object%@%"models")[[1]]]][[1]])
+}
+
+#' @export
+equation.mdl_ts <- function(object, ...){
+  if(any(!map_lgl(object$transformation, compose(is.name, body)))){
+    abort("Cannot display equations containing transformations.")
+  }
+  equation(object[["fit"]])
+}
