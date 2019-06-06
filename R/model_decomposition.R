9d3ace7ccb76c417a811e782aca5efd605b23d2e
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-06 16:02:17 +1000

    Fixed generic consistency for report.model_combination

--- R/model_decomposition.R
+++ R/model_decomposition.R
@@ -142,7 +142,7 @@ model_sum.decomposition_model <- function(x){
 }
 
 #' @export
-report.model_combination <- function(x){
+report.model_combination <- function(object, ...){
   comb_expr <- traverse(
     x, .f = function(resp, comb) eval(expr(substitute(!!comb, resp))),
     .h = function(x) if(is_model(x)) x[["response"]][[1]] else x%@%"combination",
