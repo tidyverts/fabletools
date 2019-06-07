b6c2cf9755574690c2905208f64be77174dcc5fd
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-07 13:56:26 +1000

    Added decomposition to test objects

--- tests/testthat/setup-data.R
+++ tests/testthat/setup-data.R
@@ -20,8 +20,8 @@ mbl_mv <- lung_deaths_wide %>%
   model(var = fable::VAR(vars(mdeaths, fdeaths) ~ fourier(K = 4)))
 fbl_mv <- mbl_mv %>% forecast
 
-# dbl <- us_deaths %>%
-#   feasts::STL(value)
-# 
-# dbl_multi <- us_deaths %>%
-#   feasts::STL(value)
+dbl <- us_deaths %>%
+  feasts::STL(value)
+
+dbl_multi <- us_deaths %>%
+  feasts::STL(value)
