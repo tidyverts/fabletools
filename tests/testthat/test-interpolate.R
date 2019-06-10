a99d0656aff98bcaaa6f290cff95e2e9b68475b8
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-10 10:38:33 +1000

    Added tests for interpolate()

--- tests/testthat/test-interpolate.R
+++ tests/testthat/test-interpolate.R
@@ -1,10 +1,10 @@
 context("test-interpolate.R")
 
 test_that("Test interpolation", {
-  # NA_pos <- sample(seq_len(NROW(us_deaths)), 10)
-  # us_deaths$value[NA_pos] <- NA 
-  # mbl <- us_deaths %>%
-  #   fable::LM(value ~ trend() + season())
-  # interpolated <- interpolate(mbl)
-  # expect_true(all(!is.na(interpolated$value)))
+  NA_pos <- c(0, sample(seq_len(NROW(us_deaths)), 10), NROW(us_deaths))
+  us_deaths$value[NA_pos] <- NA
+  mbl_miss <- us_deaths %>%
+    model(fable::TSLM(value ~ trend() + season()))
+  interpolated <- interpolate(mbl_miss, us_deaths)
+  expect_true(all(!is.na(interpolated$value)))
 })
