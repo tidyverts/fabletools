e2a3f81a81c45a13515e185785e00935b6423ffb
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-11 23:03:43 +1000

    Added components() docs
    #24

--- R/components.R
+++ R/components.R
@@ -1,3 +1,26 @@
+#' Extract components from a fitted model
+#' 
+#' Allows you to extract elements of interest from the model which can be
+#' useful in understanding how they contribute towards the overall fitted values.
+#' 
+#' A dable will be returned, which will allow you to easily plot the components
+#' and see the way in which components are combined to give forecasts.
+#' 
+#' @param object A mable.
+#' @param ... Other arguments passed to methods.
+#' 
+#' @examples 
+#' library(fable)
+#' library(tsibbledata)
+#' 
+#' # Forecasting with an ETS(M,Ad,A) model to Australian beer production
+#' aus_production %>%
+#'   model(ets = ETS(log(Beer) ~ error("M") + trend("Ad") + season("A"))) %>% 
+#'   components() %>% 
+#'   autoplot()
+#'   
+#'
+#' @rdname components
 #' @export
 components.mdl_df <- function(object, ...){
   object <- gather(object, ".model", ".fit", !!!syms(object%@%"models"))
@@ -11,6 +34,7 @@ components.mdl_df <- function(object, ...){
            seasons = attrs[["seasons"]], aliases = attrs[["aliases"]])
 }
 
+#' @rdname components
 #' @export
 components.mdl_ts <- function(object, ...){
   components(object$fit, ...)
