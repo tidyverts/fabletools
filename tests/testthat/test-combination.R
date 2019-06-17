b4cb1b528049af0cf122a241b90740bb62efa7c5
Mitchell <mitch.ohara-wild@monash.edu>
2019-06-17 23:47:15 +0300

    Added test for inv_var ensemble weights

--- tests/testthat/test-combination.R
+++ tests/testthat/test-combination.R
@@ -32,12 +32,20 @@ test_that("Combination modelling", {
       snaive = fable::SNAIVE(value),
       rw = fable::RW(value ~ drift()),
       cmbn = (fable::SNAIVE(value) + fable::RW(value ~ drift()))/2
-    )
+    ) %>% 
+    mutate(combination_ensemble(snaive, rw, weights = "inv_var"))
   fbl_snaive <- forecast(select(mbl_cmbn, 1))
   fbl_rw <- forecast(select(mbl_cmbn, 2))
   fbl_cmbn <- forecast(select(mbl_cmbn, 3))
+  fbl_wt_cmbn <- forecast(select(mbl_cmbn, 4))
   expect_equal(
     fbl_cmbn$value,
     (fbl_snaive$value + fbl_rw$value)/2
+  )
+  expect_failure(
+    expect_equal(
+      fbl_wt_cmbn$value,
+      fbl_cmbn$value
+    )
   )
 })
