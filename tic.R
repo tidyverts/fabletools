0f7840dcd6b52ccb4f41b1f4aea7da89367b7847
Mitchell <mitch.ohara-wild@monash.edu>
2019-08-27 17:32:34 +1000

    Test with dev tibble on travis

--- tic.R
+++ tic.R
@@ -2,7 +2,7 @@ do_package_checks()
 
 if (Sys.getenv("DEV_VERSIONS") != "") {
   get_stage("install") %>%
-    add_step(step_install_github(c("r-lib/rlang", "tidyverse/dplyr", "tidyverse/tidyr")))
+    add_step(step_install_github(c("r-lib/rlang", "tidyverse/dplyr", "tidyverse/tidyr", "tidyverse/tibble")))
 }
 
 if (Sys.getenv("BUILD_PKGDOWN") != "" && ci()$get_branch() == "master") {
