c744cf03268b91e471f180fbb8a2eefa7d269491
Mitchell <mitch.ohara-wild@monash.edu>
2019-09-10 20:21:51 +1000

    Fix handling of unnamed features

--- R/features.R
+++ R/features.R
@@ -18,7 +18,8 @@ features_impl <- function(.tbl, .var, features, ...){
       fn_safe <- safely(fn, tibble(.rows = 1))
       res <- transpose(map(key_dt[[".rows"]], function(i){
         out <- do.call(fn_safe, c(list(x[i]), dots[intersect(names(fmls), names(dots))]))
-        if(is.null(names(out))) names(out) <- rep(".?", length(out))
+        if(is.null(names(out[["result"]]))) 
+          names(out[["result"]]) <- rep(".?", length(out[["result"]]))
         out
       }))
       err <- compact(res[["error"]])
