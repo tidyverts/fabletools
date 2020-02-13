63ae8c90b12c3a86061679b260389a1d550fb342
Hong Ooi <hongooi@microsoft.com>
2020-02-14 06:01:50 +1100

    forecast doc

--- R/forecast.R
+++ R/forecast.R
@@ -28,6 +28,17 @@ forecast <- function(object, ...){
 #' Refer to `vignette("transformations", package = "fable")` for more details.
 #' @param ... Additional arguments for forecast model methods.
 #' 
+#' @return
+#' A tsibble containing the following columns:
+#' - `.model`: The name of the model used to obtain the forecast. Taken from
+#'   the names of the `...` arguments, if provided. If no names were provided,
+#'   this is contructed from the class and formula of the model.
+#' - The point forecast, which by default is the mean. The name of this column
+#'   will be the same as the dependent variable in the model(s).
+#' - `.distribution`. A column of objects of class `dist`, representing the
+#'   statistical distribution of the forecast in the given time period.
+#' - All columns in `new_data`, excluding those whose names conflict with the
+#'   above.
 #' @examples 
 #' if (requireNamespace("fable", quietly = TRUE)) {
 #' library(fable)
