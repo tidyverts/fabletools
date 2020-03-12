635c6429fc0e0c254408deb7b6599cad275ff30f
Mitchell <mail@mitchelloharawild.com>
2020-03-13 00:11:07 +1100

    Revert "xreg parser test fix"
    This reverts commit 06c85b8a88452acb3ebe15493fdb2af4c50f01b8.

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -11,8 +11,7 @@ test_that("Model parsing variety", {
   
   # Parse xreg
   parse_xreg <- model(us_deaths, specials(value ~ value + log(value)))
-  expect_identical(parse_xreg[[1]][[1]]$fit$xreg, 
-                   list("xreg(value)", "xreg(log(value))"))
+  expect_identical(parse_xreg[[1]][[1]]$fit$xreg[[1]], "xreg(value, log(value))")
   
   # Parse special
   parse_log5 <- model(us_deaths, specials(value ~ log5(value)))
