b01fe5291352b79c69432c0c5c9c3ce848d9340e
Mitchell <mail@mitchelloharawild.com>
2020-05-01 13:52:38 +1000

    Added dplyr dev methods for fbl_ts

---
+++ R/dplyr-fable.R
@@ -0,0 +1,26 @@
+#' @export
+dplyr_row_slice.fbl_ts <- function(data, i, ..., preserve = FALSE) {
+  res <- NextMethod()
+  as_fable(res, response = response_vars(data), distribution = distribution_var(data))
+}
+
+#' @export
+dplyr_row_slice.grouped_fbl <- dplyr_row_slice.fbl_ts
+
+#' @export
+dplyr_col_modify.fbl_ts <- function(data, cols) {
+  res <- NextMethod()
+  as_fable(res, response = response_vars(data), distribution = distribution_var(data))
+}
+
+#' @export
+dplyr_col_modify.grouped_fbl <- dplyr_col_modify.fbl_ts
+
+#' @export
+dplyr_reconstruct.fbl_ts <- function(data, template) {
+  res <- NextMethod()
+  as_fable(res, response = response_vars(template), distribution = distribution_var(template))
+}
+
+#' @export
+dplyr_reconstruct.grouped_fbl <- dplyr_reconstruct.fbl_ts
