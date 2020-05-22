0aa014a9b444885dc9ba03f42feb2d3309a7107c
Mitchell <mail@mitchelloharawild.com>
2020-05-22 20:21:06 +1000

    Added experimental aggregation code for aggregating large datasets

--- R/aggregate.R
+++ R/aggregate.R
@@ -29,7 +29,7 @@ aggregate_key <- function(.data, .spec, ...){
 }
 
 #' @export
-aggregate_key.tbl_ts <- function(.data, .spec = NULL, ..., no_idx_grp = FALSE){
+aggregate_key.tbl_ts <- function(.data, .spec = NULL, ..., dev = FALSE){
   .spec <- enexpr(.spec)
   if(is.null(.spec)){
     message(
@@ -44,7 +44,6 @@ aggregate_key.tbl_ts <- function(.data, .spec = NULL, ..., no_idx_grp = FALSE){
   key_comb <- attr(tm, "factors")
   key_vars <- rownames(key_comb)
   key_comb <- map(split(key_comb, col(key_comb)), function(x) key_vars[x!=0])
-  
   if(attr(tm, "intercept")){
     key_comb <- c(list(chr()), key_comb)
   }
@@ -52,11 +51,22 @@ aggregate_key.tbl_ts <- function(.data, .spec = NULL, ..., no_idx_grp = FALSE){
   idx <- index2_var(.data)
   intvl <- interval(.data)
   kd <- key_data(.data)
+  cn <- colnames(.data)
   has_varied_index <- any(has_gaps(.data, .full = TRUE)[[".gaps"]]) && !is_ordered(.data)
   .data <- as_tibble(.data)
   
   kv <- unique(unlist(key_comb, recursive = FALSE))
-  if(!no_idx_grp){
+  if(dev){
+    .data <- map(unname(key_comb), function(x){
+      agg <- summarise_alt(.data, ..., 
+                           .grps = group_by_alt(.data, !!sym(idx), !!!syms(x)))
+      agg[x] <- map(agg[x], agg_vec)
+      agg_keys <- setdiff(kv, x)
+      agg[agg_keys] <- rep(list(agg_vec(NA_character_, aggregated = TRUE)), length(agg_keys))
+      agg
+    })
+    .data <- vctrs::vec_rbind(!!!.data)
+  } else {
     agg_dt <- map(unname(key_comb), function(x){
       gd <- group_data(group_by(.data, !!sym(idx), !!!set_names(map(x, function(.) expr(agg_vec(!!sym(.)))), x)))
       agg_keys <- setdiff(kv, x)
@@ -65,32 +75,13 @@ aggregate_key.tbl_ts <- function(.data, .spec = NULL, ..., no_idx_grp = FALSE){
       gd[c(idx, kv, ".rows")]
     })
     agg_dt <- vctrs::vec_rbind(!!!agg_dt)
-  } else {
-    idx_val <- .data[[idx]][kd[[".rows"]][[1]]]
-    split_rows <- function(x){
-      x <- matrix(unlist(x, recursive = FALSE, use.names = FALSE), ncol = length(x))
-      lapply(seq_len(nrow(x)), function(i) x[i,,drop=TRUE])
-    }
-    agg_dt <- map(unname(key_comb), function(x){
-      cat(x, "\n")
-      gd <- group_by(kd, !!!set_names(map(x, function(.) expr(agg_vec(!!sym(.)))), x))
-      # if(n_groups(gd) == nrow(gd)){
-      #   gd <- .data
-      #   gd
-      # }
-      gd <- summarise(gd, !!idx := idx_val, .rows = split_rows(.rows))
-      agg_keys <- setdiff(kv, x)
-      agg_cols <- rep(list(agg_vec(NA_character_, aggregated = TRUE)), length(agg_keys))
-      gd[agg_keys] <- agg_cols
-      gd[c(idx, kv, ".rows")]
-    })
-    agg_dt <- vctrs::vec_rbind(!!!agg_dt)
+    .data <- dplyr::new_grouped_df(.data, groups = agg_dt)
+    .data <- summarise(.data, ...)
   }
   
-  .data <- dplyr::new_grouped_df(.data, groups = agg_dt)
+  # Re-order columns into index, keys, values order
+  .data <- .data[c(idx, kv, setdiff(colnames(.data), c(idx,kv)))]
   
-  # Compute aggregates
-  .data <- summarise(.data, ...)
   key_dt <- group_data(group_by(.data, !!!syms(kv)))
   .data <- ungroup(.data)
   
@@ -120,7 +111,6 @@ aggregate_index <- function(.data, .times, ...){
 aggregate_index.tbl_ts <- function(.data, .times = NULL, ...){
   warn("Temporal aggregation is highly experimental. The interface will be refined in the near future.")
   
-  browser()
   require_package("lubridate")
   idx <- index(.data)
   kv <- key_vars(.data)
