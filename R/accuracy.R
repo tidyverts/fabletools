1e193d5236c0b157ed96b82eaa02ed649c0a6b72
Mitchell <mail@mitchelloharawild.com>
2020-05-29 14:07:44 +1000

    Fix ambiguous pivot column specification in mv accuracy.fbl_ts

--- R/accuracy.R
+++ R/accuracy.R
@@ -383,10 +383,10 @@ accuracy.fbl_ts <- function(object, data, measures = point_accuracy_measures, ..
   if(mv <- length(resp) > 1){
     aug <- object <- tidyr::pivot_longer(
       transmute(object, mean(!!dist), .dist = !!dist, !!!syms(intersect(by, colnames(object)))),
-      resp, names_to = ".response", values_to = ".fc")
+      !!resp, names_to = ".response", values_to = ".fc")
     aug_dt <- data <- tidyr::pivot_longer(
       transmute(data, !!index(data), !!!syms(resp)),
-      resp, names_to = ".response", values_to = ".actual")
+      !!resp, names_to = ".response", values_to = ".actual")
     resp <- ".actual"
   } else {
     aug <- transmute(object, .fc = mean(!!dist), .dist = !!dist, !!!syms(by))
