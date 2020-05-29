8f21972e7a7c61fa89281233c9987dbebdaf2f7c
Mitchell <mail@mitchelloharawild.com>
2020-05-29 15:45:28 +1000

    Improved agg_vec vctrs methods for comparison and casting
    Resolves #204

--- R/aggregate.R
+++ R/aggregate.R
@@ -70,7 +70,7 @@ aggregate_key.tbl_ts <- function(.data, .spec = NULL, ...){#, dev = FALSE){
   agg_dt <- map(unname(key_comb), function(x){
     gd <- group_data(group_by(.data, !!sym(idx), !!!set_names(map(x, function(.) expr(agg_vec(!!sym(.)))), x)))
     agg_keys <- setdiff(kv, x)
-    agg_cols <- rep(list(agg_vec("", aggregated = TRUE)), length(agg_keys))
+    agg_cols <- rep(list(agg_vec(NA, aggregated = TRUE)), length(agg_keys))
     gd[agg_keys] <- agg_cols
     gd[c(idx, kv, ".rows")]
   })
@@ -200,7 +200,18 @@ NULL
 vec_ptype2.agg_vec <- function(x, y, ...) UseMethod("vec_ptype2.agg_vec", y)
 #' @rdname aggregation-vctrs
 #' @export
-vec_ptype2.agg_vec.agg_vec <- function(x, y, ...) agg_vec()
+vec_ptype2.agg_vec.agg_vec <- function(x, y, ...) {
+  x <- vec_data(x)[["x"]]
+  y <- vec_data(y)[["x"]]
+  ptype <- if(!is_logical(x) && !is_logical(y)) {
+    vec_ptype2(x, y)
+  } else if (is_logical(x)) {
+    y
+  } else {
+    x
+  }
+  agg_vec(ptype)
+}
 #' @rdname aggregation-vctrs
 #' @export
 vec_ptype2.agg_vec.default <- function(x, y, ...) agg_vec()
@@ -222,7 +233,11 @@ vec_ptype_abbr.agg_vec <- function(x, ...) {
 vec_cast.agg_vec <- function(x, to, ...) UseMethod("vec_cast.agg_vec")
 #' @rdname aggregation-vctrs
 #' @export
-vec_cast.agg_vec.agg_vec <- function(x, to, ...) x
+vec_cast.agg_vec.agg_vec <- function(x, to, ...) {
+  x <- vec_proxy(x)
+  if(all(x$agg)) x$x <- rep_len(vec_cast(NA, vec_proxy(to)$x), length(x$x))
+  vec_restore(x, to)
+}
 #' @rdname aggregation-vctrs
 #' @export
 vec_cast.agg_vec.default <- function(x, to, ...) agg_vec(x)
@@ -234,8 +249,9 @@ vec_cast.character.agg_vec <- function(x, to, ...) trimws(format(x))
 
 #' @rdname aggregation-vctrs
 #' @export
-vec_proxy_compare.agg_vec <- function(x, to, ...) new_tibble(vec_data(x), nrow = vec_size(x))
-
+vec_proxy_compare.agg_vec <- function(x, ...) {
+  vec_proxy(x)[c(2,1)]
+}
 
 #' Is the element an aggregation of smaller data
 #' 
