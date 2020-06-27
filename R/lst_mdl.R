337d0a5fce7f6624375f5a362820f4d64b26cf3a
Mitchell <mail@mitchelloharawild.com>
2020-06-27 10:44:56 +1000

    Added self cast methods for lst_mdl

--- R/lst_mdl.R
+++ R/lst_mdl.R
@@ -1,4 +1,4 @@
-list_of_models <- function(x){
+list_of_models <- function(x = list()){
   vctrs::new_vctr(x, class = "lst_mdl")
 }
 
@@ -13,3 +13,16 @@ format.lst_mdl <- function(x, ...){
 
 #' @export
 vec_cast.character.lst_mdl <- function(x, to, ...) format(x)
+
+#' @export
+vec_ptype2.lst_mdl.lst_mdl <- function(x, y, ...){
+  list_of_models()
+}
+
+#' @export
+vec_cast.lst_mdl.lst_mdl <- function(x, to, ...){
+  if(!identical(class(x), class(to))){
+    abort("Cannot combine model lists with different reconciliation strategies.")
+  }
+  x
+}
