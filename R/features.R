2652e1f375caeddbeea6cc44b9e733a97eab32f9
Mitch <mitch.ohara-wild@monash.edu>
2021-01-04 12:13:56 +1100

    Improved features() examples

--- R/features.R
+++ R/features.R
@@ -90,7 +90,7 @@ features_impl <- function(.tbl, .var, features, ...){
 #' @param .var,.vars The variable(s) to compute features on
 #' @param features A list of functions (or lambda expressions) for the features to compute. [`feature_set()`] is a useful helper for building sets of features.
 #' @param .predicate A predicate function (or lambda expression) to be applied to the columns or a logical vector. The variables for which .predicate is or returns TRUE are selected.
-#' @param ... Additional arguments to be passed to each feature. These arguments will only be passed to features which use it in their formal arguments ([`base::formals()`]), and not via their `...`. While passing `na.rm = TRUE` to [`stats::var()`] will work, it will not for [`base::mean()`] as its formals are `x` and `...`. To more precisely pass inputs to each function, you can use lambdas in the list of features (`~ mean(., na.rm = TRUE)`).
+#' @param ... Additional arguments to be passed to each feature. These arguments will only be passed to features which use it in their formal arguments ([`base::formals()`]), and not via their `...`. While passing `na.rm = TRUE` to [`stats::var()`] will work, it will not for [`base::mean()`] as its formals are `x` and `...`. To more precisely pass inputs to each function, you should use lambdas in the list of features (`~ mean(., na.rm = TRUE)`).
 #'
 #' @seealso [`feature_set()`]
 #'
@@ -104,6 +104,11 @@ features_impl <- function(.tbl, .var, features, ...){
 #' if(requireNamespace("feasts")) library(feasts)
 #' tourism %>% 
 #'   features(Trips, features = feature_set(tags = "autocorrelation"))
+#' 
+#' # Best practice is to use anonymous functions for additional arguments
+#' tourism %>% 
+#'   features(Trips, list(~ quantile(., probs=seq(0,1,by=0.2))))
+#'
 #'
 #' @export
 features <- function(.tbl, .var, features, ...){
