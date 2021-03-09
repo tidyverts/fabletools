5c3e995d469a7de3016360fe1ae7e6264f87fa1f
mitchelloharawild <mitch.ohara-wild@monash.edu>
2021-03-10 00:40:20 +1100

    Added special_xreg() for unified handling of exogenous regressors in models

--- R/specials.R
+++ R/specials.R
@@ -17,3 +17,39 @@ new_specials <- function(..., .required_specials = NULL, .xreg_specials = NULL){
             xreg_specials = .xreg_specials,
             class="fable_specials")
 }
+
+#' Special for producing a model matrix of exogenous regressors
+#' 
+#' @param default_intercept Should an intercept be included if not specified in the model?
+#' 
+#' @importFrom stats model.frame
+#' @export
+special_xreg <- function(default_intercept = TRUE) {
+  out <- function(...) {
+    default_intercept <- !!default_intercept
+    dots <- enexprs(...)
+    constants <- map_lgl(dots, inherits, "numeric")
+    constant_specified <- any(map_lgl(dots[constants], `%in%`, c(-1, 0, 1)))
+    if(!default_intercept && !constant_specified) {
+      dots <- c(dots, list(0))
+    }
+    
+    model_formula <- new_formula(
+      lhs = NULL,
+      rhs = reduce(dots, function(.x, .y) call2("+", .x, .y))
+    )
+    
+    env <- map(enquos(...), get_env)
+    env[map_lgl(env, compose(is_empty, env_parents))] <- NULL
+    env <- if (!is_empty(env)) get_env(env[[1]]) else base_env()
+    
+    xreg <- model.frame(model_formula, data = env, na.action = stats::na.pass)
+    mm <- model.matrix(terms(xreg), xreg)
+    if (NROW(mm) == 0 && identical(colnames(mm), "(Intercept)")) {
+      return(matrix(data = 1, nrow = NROW(self$data), dimnames = list(NULL, "(Intercept)")))
+    }
+    mm
+  }
+  body(out)[[2]] <- expr(default_intercept <- !!default_intercept)
+  out
+}
