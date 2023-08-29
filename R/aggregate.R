aea6944080465dec45c992e0e3ac8e8467810060
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-08-29 11:52:12 +1000

    WIP graph vector aggregation class

--- R/aggregate.R
+++ R/aggregate.R
@@ -458,3 +458,67 @@ scale_type.agg_vec <- function(x) {
 #   }
 #   out
 # }
+
+#' @export
+graph_vec <- function(x = factor(), edges = matrix(integer(), nrow = 0, ncol = 2)) {
+  vctrs::vec_assert(edges, matrix(integer(), nrow = 0, ncol = 2))
+  stopifnot(inherits(x,"factor"))
+  # stopifnot(c("from", "to") %in% names(edges))
+  # if(inherits(x, "vctrs_rcrd")) {
+  #   structure(x, e = edges, class = c("graph_vec", class(x)))
+  # } else{
+  structure(x, levels = levels(x), e = edges, class = c("graph_vec", "vctrs_vctr", "factor"))
+  # }
+}
+
+#' @export
+format.graph_vec <- function(x, ...){
+  class(x) <- "factor"
+  format(x, ...)
+}
+
+#' @export
+levels.graph_vec <- function(x, ...) {
+  attr(x, "levels")
+}
+
+#' @rdname aggregation-vctrs
+#' @importFrom vctrs vec_ptype2
+#' @method vec_ptype2 graph_vec
+#' @export
+vec_ptype2.graph_vec <- function(x, y, ...) UseMethod("vec_ptype2.graph_vec", y)
+
+#' @rdname aggregation-vctrs
+#' @export
+vec_ptype2.graph_vec.default <- function(x, y, ...) graph_vec()
+
+#' @rdname aggregation-vctrs
+#' @method vec_cast graph_vec
+#' @export
+vec_cast.graph_vec <- function(x, to, ...) UseMethod("vec_cast.graph_vec")
+#' @rdname aggregation-vctrs
+#' @export
+vec_cast.graph_vec.graph_vec <- function(x, to, ...) {
+  x <- vec_proxy(x)
+  if(all(x$agg)) x$x <- vec_rep(vec_cast(NA, vec_proxy(to)$x), length(x$x))
+  vec_restore(x, to)
+}
+#' @rdname aggregation-vctrs
+#' @export
+vec_cast.graph_vec.default <- function(x, to, ...) graph_vec(x)
+
+#' @rdname aggregation-vctrs
+#' @export
+vec_ptype_abbr.graph_vec <- function(x, ...) {
+  "graph"
+  # class(x) <- class(x)[-1]
+  # paste0(vec_ptype_abbr(x, ...), "'")
+}
+
+#' @export
+node_is_child <- function(x, node, ...){
+  stopifnot(inherits(x, "graph_vec"))
+  g <- graph_from_edgelist(attr(x, "e"))
+  unclass(x) %in% subcomponent(g, match(node, levels(x)), mode = "out")
+}
+
