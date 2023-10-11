5d1350627bdb50b24f3b9ad4cf0c27674cbce603
azul <fede.garza.ramirez@gmail.com>
2023-10-11 15:45:03 -0600

    feat: fix index of bottom series (#362)

--- R/reconciliation.R
+++ R/reconciliation.R
@@ -242,10 +242,9 @@ forecast.lst_topdwn_mdl <- function(object, key_data,
   agg_data <- build_key_data_smat(key_data)
   S <- matrix(0L, nrow = length(agg_data$agg), ncol = max(vec_c(!!!agg_data$agg)))
   S[length(agg_data$agg)*(vec_c(!!!agg_data$agg)-1) + rep(seq_along(agg_data$agg), lengths(agg_data$agg))] <- 1L
-  
   # Identify top and bottom level
   top <- which.max(rowSums(S))
-  btm <- which(rowSums(S) == 1L)
+  btm <- agg_data$leaf
   
   kv <- names(key_data)[-ncol(key_data)]
   agg_shadow <- as_tibble(map(key_data[kv], is_aggregated))
